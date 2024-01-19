9d0b75fd662d0e0cd2852dc2d5ce95c85b00e9e2
Romainalex <romainalex.hoarau@gmail.com>
2024-01-19 09:27:44 +0200

    Ajout de PNJ.gd \n+ ajout du fichier staruml car \n\tBORDEL C'EST SUPER DUR SANS AVOIR PREPARE LE PROJET AVANT

---
+++ Scene/Actor/PNJ/PNJ.gd
@@ -0,0 +1,31 @@
+extends Actor
+class_name PNJ
+
+
+
+#### ACCESSORS ####
+
+
+
+
+
+#### BUILT-IN ####
+
+func _ready() -> void:
+	pass
+
+
+
+#### LOGICS ####
+
+# Masquage de la fonction hurt() de Actor.gd pour rendre les PNJ invulnérable
+func hurt(damage: int) -> void:
+	pass
+
+func interact() -> void:
+	pass
+
+
+#### SIGNAL RESPONSES ####
+
+
