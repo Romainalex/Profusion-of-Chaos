8e3357639792960bf20d44df23ef1c6802c8f3e3
Romain_Hoarau <romainalex.hoarau@gmail.com>
2024-03-22 09:35:57 +0200

    debug

--- Scene/Actor/PNJ/PNJ.gd
+++ Scene/Actor/PNJ/PNJ.gd
@@ -27,7 +27,7 @@ func _ready() -> void:
 #### LOGICS ####
 
 # Masquage de la fonction hurt() de Actor.gd pour rendre les PNJ invulnérable
-func hurt(_damage: int) -> void:
+func hurt(damage_data: DamageData) -> void:
 	pass
 
 func interact() -> void:
