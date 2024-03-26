010c3871e23f1f33c5c42bd02be07b6c6316d943
Poupoucoincoin <poupoucoincoin@gmail.com>
2024-03-26 18:56:46 +0400

    debut d'ennemi j'y crois inchallah ça passe

--- Scene/Actor/Ennemy/enemy.gd
+++ Scene/Actor/Ennemy/enemy.gd
@@ -4,15 +4,15 @@ class_name ennemy
 var player_chase = false
 var player = null
 
-func _physics_process(_delta):
+func _physics_process(delta):
 	if player_chase:
 		position += (player.position - position)/speed
 
 func _on_detection_area_body_entered(body):
-	player = body
+	player = body #Le joueur qui rentre est le body vers lequel doit se diriger l'ennemi
 	player_chase = true
 
 
-func _on_detection_area_body_exited(_body):
+func _on_detection_area_body_exited(body):
 	player = null
 	player_chase = false
